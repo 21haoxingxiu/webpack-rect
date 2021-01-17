e7d9469741c53726dfd3e74cd229aaa8c0493f98
21haoxingxiu <1047918517@qq.com>
2021-01-17 20:00:08 +0800

    init

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+/*
+ * @Description: new file
+ * @Autor: zhan
+ * @Date: 2021-01-17 11:00:10
+ * @LastEditors: zhan
+ * @LastEditTime: 2021-01-17 11:00:10
+ */
+let num: number = 888
+
+document.title = 'ming'
